6665b1afffbf650bf78fc7f4d12146527fad6131
Rasmus Schlünsen <rasmus@clovrlabs.com>
2025-05-06 00:12:32 +0200

    chore: Update homebrew formula

--- Formula/radio-cli.rb
+++ Formula/radio-cli.rb
@@ -7,18 +7,32 @@ class RadioCli < Formula
   head "https://github.com/schlunsen/radiocli.git", branch: "master"
 
   depends_on "rust" => :build
-  depends_on "mpv"
+  depends_on "mpv" # Required dependency for audio playback
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    # The main code is in the radio_cli directory
+    cd "radio_cli" do
+      system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    end
+
     # Install shell completions if you add them later
-    # bash_completion.install "completions/radio-cli.bash"
-    # zsh_completion.install "completions/radio-cli.zsh"
-    # fish_completion.install "completions/radio-cli.fish"
+    # bash_completion.install "completions/radio_cli.bash"
+    # zsh_completion.install "completions/radio_cli.zsh"
+    # fish_completion.install "completions/radio_cli.fish"
   end
 
   test do
     # Add a test to make sure the binary runs at the basic level
     assert_match "RadioCLI", shell_output("#{bin}/radio_cli --version")
+  end
+
+  # Show a message after installation to inform users about MPV
+  def caveats
+    <<~EOS
+      Radio CLI uses mpv for audio playback, which has been automatically installed as a dependency.
+      
+      To start listening to radio stations, simply run:
+        radio_cli
+    EOS
   end
 end

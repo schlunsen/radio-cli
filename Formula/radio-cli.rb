b92970dca22ea9244202ab8f89322521027f6be9
Rasmus Schlünsen <rasmus@clovrlabs.com>
2025-05-06 01:58:19 +0200

    Update source SHA256 for v0.9.1

--- Formula/radio-cli.rb
+++ Formula/radio-cli.rb
@@ -2,14 +2,14 @@ class RadioCli < Formula
   desc "Terminal-based internet radio player with visualizations"
   homepage "https://github.com/schlunsen/radio-cli"
   url "https://github.com/schlunsen/radio-cli/archive/refs/tags/v0.9.1.tar.gz"
-  sha256 "REPLACE_AFTER_PUSHING_TAG"
+  sha256 "acf06a6a88f3a669c477282c5490baab0712edb61617f3d5e9b79ccec47cbf2f"
   license "MIT"
   head "https://github.com/schlunsen/radio-cli.git", branch: "master"
   
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/schlunsen/radio-cli/releases/download/v0.9.1/radio_cli-macos-amd64"
-      sha256 "REPLACE_AFTER_PUSHING_TAG"
+      sha256 "acf06a6a88f3a669c477282c5490baab0712edb61617f3d5e9b79ccec47cbf2f"
     end
     # Add ARM support when available
     # if Hardware::CPU.arm?

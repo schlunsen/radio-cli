a7a69b2f329031ad07a5f4519d2f203730b34cf4
Rasmus Schlünsen <rasmus@clovrlabs.com>
2025-05-06 16:42:11 +0200

    chore: Remove unneeded files

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // This build script does nothing by default
-    // In Windows CI, the workflow will add the skip_mpv feature flag
-    println!("cargo:rerun-if-changed=build.rs");
-}

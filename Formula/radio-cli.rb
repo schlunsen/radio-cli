2bc866b93bc8ce40f3a35211609bd62cec86a6a3
Rasmus Schlünsen <rasmus@clovrlabs.com>
2025-05-06 02:27:17 +0200

    Update formula for v0.9.6

--- Formula/radio-cli.rb
+++ Formula/radio-cli.rb
@@ -1,19 +1,19 @@
 class RadioCli < Formula
   desc "Terminal-based internet radio player with visualizations"
   homepage "https://github.com/schlunsen/radio-cli"
-  url "https://github.com/schlunsen/radio-cli/archive/refs/tags/v0.9.5.tar.gz"
-  sha256 "2bac7ba96dfe1b2c681cfcdd588570bf7b78e60d3486824ad0a4bfefe146a04b"
+  url "https://github.com/schlunsen/radio-cli/archive/refs/tags/v0.9.6.tar.gz"
+  sha256 "REPLACE_AFTER_PUSHING_TAG"
   license "MIT"
   head "https://github.com/schlunsen/radio-cli.git", branch: "master"
   
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/schlunsen/radio-cli/releases/download/v0.9.4/radio_cli-macos-intel.tar.gz"
-      sha256 "2bac7ba96dfe1b2c681cfcdd588570bf7b78e60d3486824ad0a4bfefe146a04b"
+      sha256 "REPLACE_AFTER_PUSHING_TAG"
     end
     if Hardware::CPU.arm?
       url "https://github.com/schlunsen/radio-cli/releases/download/v0.9.4/radio_cli-macos-apple-silicon.tar.gz"
-      sha256 "2bac7ba96dfe1b2c681cfcdd588570bf7b78e60d3486824ad0a4bfefe146a04b"
+      sha256 "REPLACE_AFTER_PUSHING_TAG"
     end
   end
 
